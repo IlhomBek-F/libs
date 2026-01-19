624ed47e2b71bdfe8e3fa362bd54a9f1047e6a9c
Ilhomjon <futurewithme1221@gmail.com>
2026-01-19 16:51:53 +0500

    initial lib app

---
+++ projects/base-form/src/lib/base-form.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'lib-base-form',
+  imports: [],
+  template: `
+    <p>
+      base-form works!
+    </p>
+  `,
+  styles: ``
+})
+export class BaseFormComponent {
+
+}
---
+++ projects/base-form/src/public-api.ts
@@ -0,0 +1,6 @@
+/*
+ * Public API Surface of base-form
+ */
+
+export * from './lib/base-form.service';
+export * from './lib/base-form.component';
